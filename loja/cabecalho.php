8cf9f84e11c56521f66af729a98133d4c849a6f7
João Pedro Capelo Machado <47401366+jpmachado99@users.noreply.github.com>
2019-02-14 17:32:50 -0200

    primeiro commit
    Upload do projeto da aula de 'PHP e MySQL I' da Alura

---
+++ loja/cabecalho.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Minha loja</title>
+	<link href="css/bootstrap.css" rel="stylesheet">
+	<link href="css/loja.css" rel="stylesheet">
+</head>
+<body>
+
+	<div class="navbar navbar-inverse navbar-fixed-top">
+		<div class="container">
+			<div class="navbar-header">
+				<a class="navbar-brand" href="index.php">Minha loja</a>
+			</div>
+
+			<div>
+				<ul class="nav navbar-nav">
+					<li><a href="produto-formulario.php">Adicionar Produtos</a></li>
+					<li><a href="produto-lista.php">Lista de Produtos</a></li>
+					<!-- <li><a href="sobre.php">Sobre</a></li> -->
+				</ul>
+			</div>
+		</div>
+	</div>
+
+	<div class="container">
+		
+		<div class="principal">
