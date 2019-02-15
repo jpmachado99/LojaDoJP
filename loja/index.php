ea28971cbb15e52b67c15984d814f21b26592f54
jpmachado99 <joao.machado5@fatec.sp.gov.br>
2019-02-15 17:31:45 -0200

    Adicionado opção de login

--- loja/index.php
+++ loja/index.php
@@ -1,3 +1,34 @@
 <?php include("cabecalho.php"); ?>
-			<h1>Bem vindo!</h1>
+<?php
+if(isset($_GET["login"]) && $_GET["login"]==true) {
+?>
+	<p class="alert-success" align="center">Logado com sucesso!</p>
+<?php
+}	?>
+<?php
+if(isset($_GET["login"]) && $_GET["login"]==false) {
+?>
+	<p class="alert-danger" align="center">Usuário ou senha inválida!</p>
+<?php
+}	?>
+
+
+		<h1 align="center">Bem vindo!</h1>
+
+		<h3 align="center">Login</h3>
+		<form action="login.php" method="post">
+			<table class="table">
+				<tr>
+					<td>Email:</td>
+					<td><input class="form-control" type="email" name="email"></td>
+				</tr>
+				<tr>
+					<td>Senha:</td>
+					<td><input class="form-control" type="password" name="senha"></td>
+				</tr>
+				<tr>
+					<td><button class="btn btn-primary">Login</button></td>
+				</tr>
+			</table>
+		</form>
 <?php include("rodape.php"); ?>
